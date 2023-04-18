6c386d55efdb865bb0a355a95c5787fcf27b3035
Jossy1238 <106344230+Jossy1238@users.noreply.github.com>
2023-04-18 04:42:52 +0400

    new changes that are due

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -10,3 +10,18 @@ document.querySelectorAll(".navigation-field").forEach(n => n.addEventListener("
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+ 
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'center',
+  contain: true,
+  freeScroll: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
